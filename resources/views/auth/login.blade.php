f50cf6771d3ec88446226af3165bafc1e7117862
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-02-24 10:00:36 +0000

    feat: ajoute les noms aux inputs dans les forms d'enregistrement et de login et l'ajoute de LoginRequest et RegisterRequest

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 <p>login</p>
 <form action="#" method="post">
-    <input type="email" name="" id="" placeholder="email">
-    <input type="password" name="" id="" placeholder="password">
+    @csrf
+    <input type="email" name="email" id="" placeholder="email">
+    <input type="password" name="password" id="" placeholder="password">
     <button type="submit">Entre</button>
 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,8 @@
 <p>register</p>
 <form action="#" method="post">
-    <input type="text" name="" id="" placeholder="nom">
-    <input type="email" name="" id="" placeholder="email">
-    <input type="password" name="" id="" placeholder="password">
-    <input type="password" name="" id="" placeholder="confirme password">
+    <input type="text" name="name" id="" placeholder="nom">
+    <input type="email" name="email" id="" placeholder="email">
+    <input type="password" name="password" id="" placeholder="password">
+    <input type="password" name="password_confirmation" id="" placeholder="confirme password">
     <button type="submit">Entre</button>
 </form>
