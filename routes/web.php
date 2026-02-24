eb863cd69d781e1c3755f5f53a1d7cacbfc13aa1
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-02-24 09:20:09 +0000

    feat: ajoute de route pour voir la page du login

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\LoginController;
+
 Route::get('/', [UserController::class , 'index']);
+Route::get('login' , [LoginController::class , 'index'])->name('view.auth.login');
