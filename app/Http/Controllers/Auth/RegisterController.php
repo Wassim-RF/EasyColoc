d4db7269fc121861e96146294921cd05a9c68773
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-02-24 11:19:14 +0000

    feat: creation du code de registerController  pour enregistrer un nouvelle utilisateur

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,35 @@
 <?php
     namespace App\Http\Controllers\Auth;
-
+    
+    use App\Services\AuthServices;
+    use App\Services\RegisterServices;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Hash;
     use App\Http\Controllers\Controller;
+    use App\Http\Requests\RegisterRequest;
 
     class RegisterController extends Controller {
         public function index() {
             return view('auth.register');
+        }
+
+        public function register(RegisterRequest $registerRequest , AuthServices $authServices , RegisterServices $registerServices) {
+            $user = $authServices->existeEmail($registerRequest->email);
+
+            if ($user) return redirect()->back()->with('error', 'Cette adresse e-mail est déjà utilisée.');
+
+            $data = [
+                'name' => $registerRequest->name,
+                'email' => $registerRequest->email,
+                'password' => $registerRequest->password
+            ];
+
+            $newUser = $registerServices->create($data);
+
+            Auth::login($newUser);
+
+            $registerRequest->session()->regenerate();
+
+            return redirect('/home')->with('success' , 'Vous creer votre compte en succe');
         }
     }
