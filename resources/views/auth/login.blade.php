0973654f45290c8588017e68c481682b567f34c3
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-02-24 09:31:42 +0000

    feat: ajoute un ppetit design pour le login le register pour tester le code d'enregitrement

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,6 @@
 <p>login</p>
+<form action="#" method="post">
+    <input type="email" name="" id="" placeholder="email">
+    <input type="password" name="" id="" placeholder="password">
+    <button type="submit">Entre</button>
+</form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,8 @@
 <p>register</p>
+<form action="#" method="post">
+    <input type="text" name="" id="" placeholder="nom">
+    <input type="email" name="" id="" placeholder="email">
+    <input type="password" name="" id="" placeholder="password">
+    <input type="password" name="" id="" placeholder="confirme password">
+    <button type="submit">Entre</button>
+</form>
